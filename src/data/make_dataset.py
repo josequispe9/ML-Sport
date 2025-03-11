0539328936910d9b8921d7aa3f11b11228e00620
josequispe9 <jose.quispe@ib.edu.ar>
2025-03-11 18:09:13 -0300

    importar archivos csv

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -7,10 +7,14 @@ from glob import glob
 
 single_file_acc = pd.read_csv('../../data/raw/MetaMotion/accelerometer_2021-03-25T14-00-00.000000Z_2021-03-25T14-30-00.000000Z.csv')
 
+single_file_gyr = pd.read_csv('../../data/raw/MetaMotion/gyroscope_2021-03-25T14-00-00.000000Z_2021-03-25T14-30-00.000000Z.csv')
+
+
 # --------------------------------------------------------------
 # List all data in data/raw/MetaMotion
 # --------------------------------------------------------------
 
+files = glob('../../data/raw/MetaMotion/*.csv')
 
 # --------------------------------------------------------------
 # Extract features from filename
