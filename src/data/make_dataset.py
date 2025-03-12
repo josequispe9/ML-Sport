1650eeecfbd5be0c1713338c3ae5496d0a842bba
josequispe9 <jose.quispe@ib.edu.ar>
2025-03-12 15:47:23 -0300

    Read all files already

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -23,16 +23,45 @@ files = glob('../../data/raw/MetaMotion/*.csv')
 data_path = "../../data/raw/MetaMotion/"
 f = files[0]
 
-participant = f.split("-")[0].replace(data_path)
+participant = f.split("-")[0].replace(data_path, "")
+label = f.split("-")[1]
+category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
 
-
-label
-category
+df = pd.read_csv(f)
+df["participant"] = participant
+df["label"] = label
+df["category"] = category
 
 
 # --------------------------------------------------------------
 # Read all files
 # --------------------------------------------------------------
+
+acc_df = pd.DataFrame()
+gyr_df = pd.DataFrame()
+
+acc_set = 1
+gyr_set = 1
+
+for f in files:
+    participant = f.split("-")[0].replace(data_path, "")
+    label = f.split("-")[1]
+    category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+
+    df = pd.read_csv(f)
+    df["participant"] = participant
+    df["label"] = label
+    df["category"] = category
+
+    if "Accelerometer" in f:
+        df["set"] = acc_set
+        acc_set += 1
+        acc_df = pd.concat([acc_df, df])
+    if "Gyroscope" in f:
+        df["set"] = gyr_set
+        gyr_set += 1
+        gyr_df = pd.concat([gyr_df, df])
+        
 
 
 # --------------------------------------------------------------
